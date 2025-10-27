c4c2d2a3c9fb4155f558adbc94c16daffae06157
marcos-valle <marcos.vallesn@gmail.com>
2025-10-26 22:15:37 -0300

    feat: add poster to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_and_belongs_to_many :categories
+  has_one_attached :poster
 
   validates :title, :synopsis, :release_year, :duration, :director, presence: true
 end
